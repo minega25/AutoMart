9c16decead576f009ae086bfe8d3739309071340
minega <minega25@gmail.com>
2019-06-06 00:42:47 +0200

    [finishes #166259570] Add mark user posted advert as sold functionality

--- src/routes/cars.js
+++ src/routes/cars.js
@@ -1,11 +1,12 @@
 import express from 'express';
 
-import { carCreatePost } from '../controllers/carController';
+import { carCreatePost, updateCarStatus } from '../controllers/carController';
 import auth from '../middleware/auth';
 
 const router = express.Router();
 
 // POST request to create car
 router.post('/', auth, carCreatePost);
+router.patch('/:car_id/status', auth, updateCarStatus);
 
 export default router;
