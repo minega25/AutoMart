0bebad8a9631498cedf7ecc3028f4ee5f683fe04
minega <minega25@gmail.com>
2019-06-06 10:29:57 +0200

    [finishes 166259575] Add view specific car by id functionality

--- src/routes/cars.js
+++ src/routes/cars.js
@@ -1,13 +1,14 @@
 import express from 'express';
 
-import { carCreatePost, updateCarStatus, updateCarPrice } from '../controllers/carController';
+import * as carController from '../controllers/carController';
 import auth from '../middleware/auth';
 
 const router = express.Router();
 
 // POST request to create car
-router.post('/', auth, carCreatePost);
-router.patch('/:car_id/status', auth, updateCarStatus);
-router.patch('/:car_id/price', auth, updateCarPrice);
+router.post('/', auth, carController.carCreatePost);
+router.patch('/:car_id/status', auth, carController.updateCarStatus);
+router.patch('/:car_id/price', auth, carController.updateCarPrice);
+router.get('/:car_id', carController.getCar);
 
 export default router;
