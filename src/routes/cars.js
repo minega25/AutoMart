ef8ec2a4d0c8ad46798c654cfac30422e6fcd963
minega <minega25@gmail.com>
2019-06-07 07:05:22 +0200

    [finishes #166259572] Add admin can delete functionality

--- src/routes/cars.js
+++ src/routes/cars.js
@@ -2,6 +2,7 @@ import express from 'express';
 
 import * as carController from '../controllers/carController';
 import auth from '../middleware/auth';
+import admin from '../middleware/admin';
 
 const router = express.Router();
 
@@ -11,5 +12,5 @@ router.patch('/:car_id/status', auth, carController.updateCarStatus);
 router.patch('/:car_id/price', auth, carController.updateCarPrice);
 router.get('/:car_id', carController.getCar);
 router.get('/', carController.getCars);
-
+router.delete('/:car_id', [auth, admin], carController.deleteCar);
 export default router;
