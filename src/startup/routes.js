296ac5de772b29526efa903d13eccaac6f376bdd
minega <minega25@gmail.com>
2019-06-07 15:47:26 +0200

    [finishes #166542305] Add api documentation

--- src/startup/routes.js
+++ src/startup/routes.js
@@ -1,11 +1,14 @@
+// import swaggerUi from 'swagger-ui-express';
 import bodyParser from 'body-parser';
 import users from '../routes/users';
 import cars from '../routes/cars';
 import orders from '../routes/orders';
+
 
 export default (app) => {
   app.use(bodyParser.json());
   app.use('/api/v1/auth', users);
   app.use('/api/v1/car', cars);
   app.use('/api/v1/order', orders);
+  // app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 };
