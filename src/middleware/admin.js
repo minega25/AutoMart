28924b2b5dca342347b17a2f0212926bbb080960
minega <minega25@gmail.com>
2019-06-07 08:45:10 +0200

    [finishes #166259571] Add view all cars by an admin user

--- src/middleware/admin.js
+++ src/middleware/admin.js
@@ -2,7 +2,7 @@
 
 // eslint-disable-next-line consistent-return
 const admin = (req, res, next) => {
-  if (!req.user.isAdmin) return res.status(403).send('Access denied.');
+  if (!req.user.isAdmin) return res.status(403).send({ status: 403, data: 'Unathorized access.' });
   next();
 };
 
