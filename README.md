162b639aaf180cc10f049646804b9ad13585d192
minega <minega25@gmail.com>
2019-06-11 09:31:02 +0200

    Implement challenge one feedback

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 [![Coverage Status](https://coveralls.io/repos/github/minega25/AutoMart/badge.svg?branch=develop)](https://coveralls.io/github/minega25/AutoMart?branch=develop)
 
+[![Maintainability](https://api.codeclimate.com/v1/badges/b6eeebdf4625b8aa7a73/maintainability)](https://codeclimate.com/github/minega25/AutoMart/maintainability)
+
 # AutoMart
 Auto Mart is an online marketplace for automobiles of diverse makes, model or body type. With Auto Mart, users can sell their cars or buy from trusted dealerships or private sellers.
 
--- UI/js/main.js
+++ UI/js/main.js
@@ -1,11 +1,28 @@
-Element.prototype.toggle = function() { 
-	this.addEventListener("click", function(event) {
-    	event.preventDefault(); 
-   	});
-    if ( this.style.display == '' || this.style.display == 'block' ) { 
-        this.style.display = 'none';
-    }
-    else{
-        this.style.display = 'block';
-   }
+// eslint-disable-next-line func-names
+Element.prototype.toggle = function () {
+  this.addEventListener('click', (event) => {
+    event.preventDefault();
+  });
+  if (this.style.display == '' || this.style.display == 'block') {
+    this.style.display = 'none';
+  } else {
+    this.style.display = 'block';
+  }
+};
+const authOut = document.getElementsByClassName('authOut')[0];
+const authIn = document.getElementsByClassName('authIn')[0];
+let isAuth = false;
+if (!isAuth) {
+  authIn.style.display = 'none';
+  authOut.style.display = 'inline-block';
+}
+function login() {
+  authIn.style.display = 'block';
+  authOut.style.display = 'none';
+  isAuth = true;
+}
+function logOut() {
+  authOut.style.display = 'inline-block';
+  authIn.style.display = 'none';
+  isAuth = false;
 }
