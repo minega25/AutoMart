ca6213106a5dc693fc15fb7293953f241da56277
minega <minega25@gmail.com>
2019-06-13 11:36:46 +0200

    [finishes #166663188] Fix user can not switch min and max query positions

--- src/middleware/admin.js
+++ src/middleware/admin.js
@@ -2,7 +2,6 @@
 
 // eslint-disable-next-line consistent-return
 const admin = (req, res, next) => {
-  console.log(req.user);
   if (!req.user.isAdmin) return res.status(403).send({ status: 403, data: 'Unathorized access.' });
   next();
 };
