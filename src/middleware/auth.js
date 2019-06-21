ffcb362c2d42a5d3fc04da9aa38ae70d861bc181
minega <minega25@gmail.com>
2019-06-21 11:19:06 +0200

    Update auth

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,7 +4,7 @@ import config from 'config';
 // eslint-disable-next-line consistent-return
 const auth = (req, res, next) => {
   const token = req.header('x-auth-token');
-  if (!token) return res.status(401).send('Access denied. No token provided');
+  if (!token) return res.status(401).send({status:401, error:'Access denied. No token provided'});
   try {
     const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
     req.user = decoded;
