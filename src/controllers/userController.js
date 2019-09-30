06ba379d949b35ee540415d473571d3047169743
Minega Shyaka Patrick <minega25@gmail.com>
2019-09-30 23:04:49 +0200

    add

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,7 @@
-import jwt from 'jsonwebtoken';
-import config from 'config';
-import bcrypt from 'bcrypt';
-import User from '../models/User';
+import jwt from "jsonwebtoken";
+import config from "config";
+import bcrypt from "bcrypt";
+import User from "../models/User";
 
 const users = new User();
 
@@ -10,10 +10,13 @@ export const userCreatePost = async (req, res) => {
   try {
     const addedUser = await users.add(req.newUser);
 
-    const userToken = jwt.sign({ id: addedUser.id, email: addedUser.email, isAdmin: addedUser.is_admin }, config.get('jwtPrivateKey'));
+    const userToken = jwt.sign(
+      { id: addedUser.id, email: addedUser.email, isAdmin: addedUser.is_admin },
+      config.get("jwtPrivateKey")
+    );
     const response = {
       status: 201,
-      message: 'User created successfully',
+      message: "User created successfully",
       data: {
         token: userToken,
         id: addedUser.id,
@@ -21,14 +24,18 @@ export const userCreatePost = async (req, res) => {
         last_name: addedUser.last_name,
         email: addedUser.email,
         address: addedUser.address,
-        is_admin: addedUser.is_admin,
-      },
+        is_admin: addedUser.is_admin
+      }
     };
-    return res.header('x-auth-token', userToken).status(201).json(response);
+    return res
+      .header("x-auth-token", userToken)
+      .header("access-control-expose-headers", "x-auth-token")
+      .status(201)
+      .json(response);
   } catch (err) {
     const response = {
       status: 409,
-      error: 'User already registered',
+      error: "User already registered"
     };
     return res.status(409).json(response);
   }
@@ -39,30 +46,44 @@ export const userLoginPost = async (req, res) => {
   if (!userRegistered) {
     const response = {
       status: 400,
-      error: 'Invalid email or password provided',
+      error: "Invalid email or password provided"
     };
     return res.status(400).json(response);
   }
 
-  const validPassword = await bcrypt.compare(req.body.password, userRegistered.password);
+  const validPassword = await bcrypt.compare(
+    req.body.password,
+    userRegistered.password
+  );
   if (!validPassword) {
     const response = {
       status: 400,
-      error: 'Invalid email or password provided',
+      error: "Invalid email or password provided"
     };
     return res.status(400).json(response);
   }
-  const userToken = jwt.sign({ id: userRegistered.id, email: userRegistered.email, isAdmin: userRegistered.is_admin }, config.get('jwtPrivateKey'));
+  const userToken = jwt.sign(
+    {
+      id: userRegistered.id,
+      email: userRegistered.email,
+      isAdmin: userRegistered.is_admin
+    },
+    config.get("jwtPrivateKey")
+  );
   const response = {
     status: 200,
-    message: 'user logged in successfully',
+    message: "user logged in successfully",
     data: {
       token: userToken,
       id: userRegistered.id,
       first_name: userRegistered.first_name,
       last_name: userRegistered.last_name,
-      email: userRegistered.email,
-    },
+      email: userRegistered.email
+    }
   };
-  return res.header('x-auth-token', userToken).status(200).json(response);
+  return res
+    .header("x-auth-token", userToken)
+    .header("access-control-expose-headers", "x-auth-token")
+    .status(200)
+    .json(response);
 };
